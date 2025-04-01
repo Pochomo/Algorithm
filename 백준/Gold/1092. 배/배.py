de25b81288d293b5f3bc740ac241edc2ea6ab3e0
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-04-01 14:17:19 +0900

    [Gold V] Title: 배, Time: 2960 ms, Memory: 110788 KB -BaekjoonHub

---
+++ 백준/Gold/1092. 배/배.py
@@ -0,0 +1,27 @@
+#1092
+import sys
+input = sys.stdin.readline
+
+n = int(input()) #무게 제한 수
+cranes = list(map(int, input().split()))
+
+m = int(input())
+boxes = list(map(int, input().split()))
+
+cranes.sort(reverse=True)
+boxes.sort(reverse=True)
+
+cnt = 0
+
+if cranes[0] < boxes[0]:
+    cnt = -1
+else:
+    while boxes:
+        for crane in cranes:
+            for box in boxes:
+                if crane >= box:
+                    boxes.remove(box)
+                    break
+        cnt += 1
+
+print(cnt)
