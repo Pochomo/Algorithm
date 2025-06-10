827600ef5ffa19c8fb7df0c1d722adc94fc31fe0
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-06-10 22:58:35 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 9.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -0,0 +1,10 @@
+def solution(seoul):
+    answer = ''
+    for i in range(len(seoul)):
+        if seoul[i] == "Kim":
+            x = i
+            break
+    
+    answer = f"김서방은 {x}에 있다" 
+    
+    return answer
