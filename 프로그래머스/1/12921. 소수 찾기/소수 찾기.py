3bb253bc4e079332d69f9b80e309d788b0d3292a
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-07-12 17:35:12 +0900

    [level 1] Title: 소수 찾기, Time: 345.52 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,10 +1,7 @@
 def solution(n):
-    prime = [True] * (n + 1)
-    prime[0] = prime[1] = False
+    num = set(range(2,n+1))
 
-    limit = int(n ** 0.5)
-    for p in range(2, limit + 1):
-        if prime[p]:
-            prime[p*p : n+1 : p] = [False] * len(range(p*p, n+1, p))
-
-    return sum(prime)
+    for i in range(2,n+1):
+        if i in num:
+            num -= set(range(2*i,n+1,i))
+    return len(num)
