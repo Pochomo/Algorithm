bc5f6e71f1f634298ad6c110812054606a59f7be
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-07-17 23:22:28 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,15 @@
+def solution(s: str) -> str:
+    result = []
+    first_word = True
+
+    for char in s:
+        if first_word:
+            # 알파벳이고 대문자로 만들고 append
+            result.append(char.upper() if char.isalpha() else char)
+            first_word = False
+        else:
+            result.append(char.lower())
+        if char == " ":
+            first_word = True
+
+    return "".join(result)
