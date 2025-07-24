d030fb559bea9864008070e1c3fae9e44d916527
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-07-24 17:04:42 +0900

    [level 2] Title: 귤 고르기, Time: 35.98 ms, Memory: 17.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -0,0 +1,21 @@
+def solution(k, tangerine):
+    time = {}
+    
+    for size in tangerine:
+        if size in time:
+            time[size] += 1
+        else:
+            time[size] = 1
+        
+    cnt = list(time.values())
+    cnt.sort(reverse = True)
+    
+    answer = 0
+    total = 0
+    for i in cnt:
+        total += i
+        answer += 1
+        if total >= k:
+            break
+    
+    return answer
